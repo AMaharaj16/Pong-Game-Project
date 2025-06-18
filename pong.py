9fd97a7ba717190b8fa421563fd7dc126d36d8d0
AMaharaj16 <aayush.maharaj16@gmail.com>
2025-06-17 18:16:22 -0600

    Added ball, ball class, and movement. No boundary check yet.

--- pong.py
+++ pong.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 pygame.display.set_caption("Pong Game in Python")
@@ -11,6 +12,8 @@ WIDTH_PADDLE = 10
 VEL_PADDLE = 5
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RADIUS_BALL = 6
+COLOUR_BALL = (255, 20, 147)
 
 class Paddle:
     def __init__(self, x, y):
@@ -31,20 +34,33 @@ class Paddle:
         elif down:
             self.y += VEL_PADDLE
 
+class Ball:
+    def __init__(self, x, y, x_vel, y_vel):
+        self.x = x
+        self.y = y
+        self.x_vel = x_vel
+        self.y_vel = y_vel
 
-def draw(window, paddles):
+    def draw(self, window):
+        pygame.draw.circle(window, COLOUR_BALL, (self.x, self.y), RADIUS_BALL)
+
+    def move(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+def draw(window, paddles, ball):
     window.fill(BLACK)
     for paddle in paddles:
         paddle.draw(window)
+    ball.draw(window)
     pygame.display.update()
-
-    
 
 def main(window):
     clock = pygame.time.Clock()
     run = True
 
     paddles = [Paddle(50,400), Paddle(750,400)]
+    ball = Ball(400,400, random.choice([-2, -1, 1, 2]), random.choice([-2, -1, 1, 2]))
     
     while run:
         clock.tick(FPS)
@@ -55,8 +71,8 @@ def main(window):
         keys = pygame.key.get_pressed()
         paddles[0].move(up=keys[pygame.K_UP], down=keys[pygame.K_DOWN])
         paddles[1].move(up=keys[pygame.K_w], down=keys[pygame.K_s])
-        draw(window, paddles)
-
+        ball.move()
+        draw(window, paddles, ball)
 
 if __name__ == "__main__":
     main(WINDOW)
